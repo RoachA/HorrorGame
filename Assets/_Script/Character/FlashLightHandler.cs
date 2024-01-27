f0f61875b7736d9fefee419fcee84a0220f44475
Roachman <anil@voodoo.o>
2024-01-27 19:22:06 +0100

    various gameplay improvements
    -flashlight manager
    -light controls
    -better contact physics

---
+++ Assets/_Script/Character/FlashLightHandler.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FlashLightHandler : MonoBehaviour
+{
+    [SerializeField] private Vector2 _offsetMultiplier = new Vector2(0.5f, 2f);
+    private Light m_light;
+    private bool m_isObscured;
+    private bool m_isOn = true;
+    private const float m_litIntensity = 9;
+    private Sequence _seq;
+    private Sequence _jitterSeq;
+    private void Start()
+    {
+        m_light = GetComponentInChildren<Light>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_isObscured) return;
+        if (other.CompareTag("Wall") == false) return;
+        m_isObscured = true;
+        
+        _seq?.Kill();
+        _seq = DOTween.Sequence();
+
+        _seq.Insert(0.1f, m_light.transform.DOLocalMoveX(_offsetMultiplier.y, 0.25f));
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (m_isObscured == false) return;
+        if (other.CompareTag("Wall") == false) return;
+        m_isObscured = false;
+        
+        _seq?.Kill();
+        _seq = DOTween.Sequence();
+
+        _seq.Insert(.25f, m_light.transform.DOLocalMoveX(_offsetMultiplier.x, 0.25f));
+    }
+    
+    public void SwitchFlashlight()
+    {
+        m_isOn = !m_isOn;
+        
+        m_light.intensity = m_isOn ? m_litIntensity : 0;
+    }
+
+    [Sirenix.OdinInspector.Button]
+    public void StartJitter()
+    {
+        _jitterSeq?.Kill(true);
+        _jitterSeq = DOTween.Sequence().SetLoops(-1);
+
+        var s = 0;
+
+        _jitterSeq.Append(DOTween.To(() => s, x => s = x, 1, .1f)
+            .OnStepComplete(() =>
+            {
+                float randomIntensity = m_isOn ? UnityEngine.Random.Range(0f, 12f) : 0;
+                m_light.intensity = randomIntensity;
+            }));
+    }
+
+    [Sirenix.OdinInspector.Button]
+    public void StopJitter()
+    {
+       _jitterSeq?.Kill(true);
+       m_light.intensity = m_litIntensity;
+    }
+}
--- Assets/_Script/Character/PlayerController.cs
+++ Assets/_Script/Character/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using Cinemachine;
 using UnityEngine;
 
@@ -20,17 +21,26 @@ public class PlayerController : MonoBehaviour
             return instance;
         }
     }
-    
-    public float Speed = 10.0f;
-    private float m_translation;
-    private float m_strafe;
+
     [SerializeField] private CinemachineVirtualCamera _vCam;
     [SerializeField] private Camera _gameCam;
+    [Header("Movement")]
+    public float WalkingSpeed = 3f;
+    public float RunningSpeed = 6f;
+    private float m_translation;
+    private float m_strafe;
+
+    [Header("Camera")]
     [SerializeField] private bool _camShakeActive = false;
     [SerializeField] private float _camShakeMultiplier = 1f;
 
+    [Header("Flash Light")]
+    [SerializeField] private FlashLightHandler _flashLight;
+
     private CinemachineBasicMultiChannelPerlin _noiseShakeComponent;
-    
+    private CharacterController m_characterController;
+    private bool _isSprinting;
+
     public CinemachineVirtualCamera VCam
     {
         get { return _vCam; }
@@ -38,30 +48,60 @@ public class PlayerController : MonoBehaviour
 
     public Camera GameCam
     {
-        get { return _gameCam;  }
+        get { return _gameCam; }
     }
-    
+
     // Use this for initialization
-    private void Start() 
+    private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         _noiseShakeComponent = _vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        m_characterController = GetComponent<CharacterController>();
     }
-	
-    // Update is called once per frame
-    private void Update()
-    {
-        m_translation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        m_strafe = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        transform.Translate(m_strafe, 0, m_translation);
 
-        if (Input.GetKeyDown("escape"))
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        HandleSprint();
+
+        // Calculate movement inputs
+        float translation = Input.GetAxis("Vertical") * (_isSprinting ? RunningSpeed : WalkingSpeed);
+        float strafe = Input.GetAxis("Horizontal") * WalkingSpeed;
+
+        // Apply movement using Rigidbody physics
+        Vector3 movement = new Vector3(strafe * Time.fixedDeltaTime, 0, translation * Time.fixedDeltaTime);
+        m_characterController.Move(transform.TransformDirection(movement));
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
         }
 
         if (_camShakeActive)
-            _noiseShakeComponent.m_AmplitudeGain = m_translation * _camShakeMultiplier + 1;
-        // _noiseShakeComponent.m_FrequencyGain = m_translation * _camShakeMultiplier * 0.25f + 0.25f;
+        {
+            // Adjust camera shake based on movement
+            _noiseShakeComponent.m_AmplitudeGain = translation * _camShakeMultiplier + 1;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _flashLight.SwitchFlashlight();
+        }
+    }
+
+    private void HandleSprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _isSprinting = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _isSprinting = false;
+        }
     }
 }
