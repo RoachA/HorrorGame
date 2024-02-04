8c162f43b73be8a4c7367a8fdfa941f5ee60eac0
Roachman <anil@voodoo.o>
2024-02-04 19:29:41 +0100

    prepares enemy

---
+++ Assets/_Script/Character/CPU/EnemyController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
